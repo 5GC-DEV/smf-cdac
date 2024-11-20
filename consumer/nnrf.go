f495859fdfe3e0a3f090e17ae70d6d529851e57d
Jayaram R <159750032+JayaramRCDAC@users.noreply.github.com>
2024-11-20 14:09:36 +0530

    PCF NF Discovery logs added

--- consumer/nnrf.go
+++ consumer/nnrf.go
@@ -247,6 +247,7 @@ func SendNrfForNfInstance(nrfUri string, targetNfType, requestNfType models.NfTy
 			smfSelf.NfStatusSubscriptions.Store(nfProfile.NfInstanceId, nrfSubData.SubscriptionId)
 		}
 	}
+	logger.ConsumerLog.Infof("SendNrfForNfInstance result: %+v", result)
 	return result, localErr
 }
 
@@ -289,6 +290,7 @@ func SendNFDiscoveryUDM() (*models.ProblemDetails, error) {
 }
 
 func SendNFDiscoveryPCF() (problemDetails *models.ProblemDetails, err error) {
+	logger.ConsumerLog.Infoln("NF discovery of PCF")
 	localVarOptionals := Nnrf_NFDiscovery.SearchNFInstancesParamOpts{}
 
 	var result models.SearchResult
@@ -302,6 +304,7 @@ func SendNFDiscoveryPCF() (problemDetails *models.ProblemDetails, err error) {
 
 	if localErr == nil {
 		logger.ConsumerLog.Debugln(result.NfInstances)
+		logger.ConsumerLog.Infoln(result.NfInstances)
 	} else {
 		apiError, ok := localErr.(openapi.GenericOpenAPIError)
 		if ok {
