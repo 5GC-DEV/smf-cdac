35340ff9fc1e9a158da995ccaf5cebc54dfc7514
AshithaCDAC <ashitha@cdac.in>
2025-03-27 09:29:04 +0000

    smf metrics added

--- producer/n1n2_data_handler.go
+++ producer/n1n2_data_handler.go
@@ -13,6 +13,9 @@ import (
 	"github.com/omec-project/openapi/models"
 	"github.com/omec-project/smf/consumer"
 	"github.com/omec-project/smf/context"
+	smf_context "github.com/omec-project/smf/context"
+	"github.com/omec-project/smf/metrics"
+	"github.com/omec-project/smf/msgtypes/svcmsgtypes"
 	"github.com/omec-project/smf/transaction"
 	"github.com/omec-project/util/httpwrapper"
 )
@@ -65,6 +68,7 @@ func HandleUpdateN1Msg(txn *transaction.Transaction, response *models.UpdateSmCo
 			}
 			if pduSessIDRelReq == pduSessIDSmCxt {
 				smContext.HandlePDUSessionReleaseRequest(m.PDUSessionReleaseRequest)
+				metrics.IncrementSessReleaseStats(smf_context.SMF_Self().NfInstanceID, string(svcmsgtypes.NsmfPDUSessionRelease), "Out", "success")
 				if buf, err := context.BuildGSMPDUSessionReleaseCommand(smContext); err != nil {
 					smContext.SubPduSessLog.Errorf("PDUSessionSMContextUpdate, build GSM PDUSessionReleaseCommand failed: %+v", err)
 				} else {
@@ -99,6 +103,7 @@ func HandleUpdateN1Msg(txn *transaction.Transaction, response *models.UpdateSmCo
 				} else {
 					response.BinaryDataN1SmMessage = buf
 				}
+				metrics.IncrementSessReleaseStats(smf_context.SMF_Self().NfInstanceID, string(svcmsgtypes.NsmfPDUSessionRelease), "Out", "failure")
 				response.JsonData.N1SmMsg = &models.RefToBinaryData{ContentId: "PDUSessionReleaseReject"}
 				smContext.ChangeState(context.SmStateModify)
 				// smContext.SubCtxLog.Debugln("PDUSessionSMContextUpdate, SMContextState Change State:", smContext.SMContextState.String())
