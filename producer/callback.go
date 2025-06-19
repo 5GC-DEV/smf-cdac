61625a997c7721b9d9d0a766ef92f052f2b3f865
anaswara <anaswara.n@cdac.in>
2025-06-19 17:50:36 +0530

    updated modify request

--- producer/callback.go
+++ producer/callback.go
@@ -271,7 +271,7 @@ func BuildAndSendQosN1N2TransferMsg(smContext *smfContext.SMContext) error {
 		SmInfo: &models.N2SmInformation{
 			PduSessionId: smContext.PDUSessionID,
 			N2InfoContent: &models.N2InfoContent{
-				NgapIeType: models.NgapIeType_PDU_RES_SETUP_REQ,
+				NgapIeType: models.NgapIeType_PDU_RES_MOD_REQ,
 				NgapData: &models.RefToBinaryData{
 					ContentId: "N2SmInformation",
 				},
