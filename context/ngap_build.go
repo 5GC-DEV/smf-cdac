fa827743d265571b68a9cd00301c2e7dc2bfa2e4
anaswara <anaswara.n@cdac.in>
2025-11-14 19:37:30 +0530

    check added

--- context/ngap_build.go
+++ context/ngap_build.go
@@ -95,15 +95,36 @@ func BuildPDUSessionResourceSetupRequestTransfer(ctx *SMContext) ([]byte, error)
 	}*/
 	// UpNode.UpfLock.RLock()
 	// defer UpNode.UpfLock.RUnlock()
-	if atomic.LoadInt32(&UpNode.Writing) == 1 {
-		logger.PduSessLog.Warn("Reading N3 while writer is active")
-	} else {
-		logger.PduSessLog.Warn("Reading N3 while writer is not active")
-	}
-	if len(UpNode.N3Interfaces) == 0 {
+	/*if len(UpNode.N3Interfaces) == 0 {
 		logger.PduSessLog.Errorf("SUPI[%s], PDUSessionID[%d]: No N3 interface available in UPF after all retries",
 			ctx.Supi, ctx.PDUSessionID)
 		return nil, fmt.Errorf("N3Interfaces is empty for UPF: %v", UpNode.N3Interfaces)
+	}*/
+	// Check if N3Interfaces is empty
+	if len(UpNode.N3Interfaces) == 0 {
+		logger.PduSessLog.Errorf(
+			"SUPI[%s], PDUSessionID[%d]: N3Interfaces empty — checking again with lock",
+			ctx.Supi, ctx.PDUSessionID,
+		)
+		// Check writer status
+		if atomic.LoadInt32(&UpNode.Writing) == 1 {
+			logger.PduSessLog.Warnf(
+				"Writer is ACTIVE while UE[%s] is reading N3Interfaces", ctx.Supi,
+			)
+		} else {
+			logger.PduSessLog.Warnf(
+				"Writer NOT active while UE[%s] is reading N3Interfaces", ctx.Supi,
+			)
+		}
+		// Acquire RLock and re-check N3Interfaces
+		UpNode.UpfLock.RLock()
+		if len(UpNode.N3Interfaces) == 0 {
+			logger.PduSessLog.Errorf("SUPI[%s], PDUSessionID[%d]: No N3 interface available in UPF after all retries",
+				ctx.Supi, ctx.PDUSessionID)
+			return nil, fmt.Errorf("N3Interfaces is empty for UPF: %v", UpNode.N3Interfaces)
+		}
+		UpNode.UpfLock.RUnlock()
+		// You can retry here or return error
 	}
 	if n3IP, err := UpNode.N3Interfaces[0].IP(ctx.SelectedPDUSessionType); err != nil {
 		logger.PduSessLog.Errorf("SUPI[%s], PDUSessionID[%d]: Failed to get N3 IP for UPF, err: %v",
