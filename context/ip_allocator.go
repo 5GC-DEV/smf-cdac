8fa919f9d0ffe01e6470b91a3d20a513ef6c2551
ReshmaJS <you@example.com>
2025-12-22 10:19:17 +0530

    ipallocate crash logs added

--- context/ip_allocator.go
+++ context/ip_allocator.go
@@ -49,7 +49,7 @@ func maskBits(mask net.IPMask) int {
 func IPAddrWithOffset(ip net.IP, offset int) net.IP {
 	retIP := make(net.IP, len(ip))
 	copy(retIP, ip)
-	logger.CtxLog.Debugf("IPAddrWithOffset: Base IP: %s, Offset: %d", ip.String(), offset)
+	logger.CtxLog.Infof("IPAddrWithOffset: Base IP: %s, Offset: %d", ip.String(), offset)
 	var carry int
 	for i := len(retIP) - 1; i >= 0; i-- {
 		if offset == 0 {
@@ -63,6 +63,7 @@ func IPAddrWithOffset(ip net.IP, offset int) net.IP {
 
 		offset /= 256
 	}
+	logger.CtxLog.Infof("IPAddrWithOffset RESULT >>> resultIP=%s remainingOffset=%d carry=%d", retIP.String(), offset, carry)
 	logger.CtxLog.Infof("IPAddrWithOffset: Resulting IP: %s", retIP.String())
 	return retIP
 }
@@ -173,12 +174,14 @@ func newIDPool(minValue int64, maxValue int64) (idPool *_IDPool) {
 		logger.CtxLog.Errorf("failed to convert SMF_COUNT to int: %v", err)
 	}
 	idPool.index = int64((smfCount-1)*2500 + 1)
+	logger.CtxLog.Infof("IDPool INIT >>> minValue=%d maxValue=%d index=%d", minValue, maxValue, idPool.index)
 	return
 }
 
 func (i *_IDPool) allocate() (id int64, err error) {
 	i.lock.Lock()
 	defer i.lock.Unlock()
+	logger.CtxLog.Infof("IDPool ALLOCATE START >>> index=%d min=%d max=%d usedCount=%d", i.index, i.minValue, i.maxValue, len(i.isUsed))
 	logger.CtxLog.Debugf("IDPool: Starting ID allocation from index %d to maxValue %d", i.index, i.maxValue)
 	smfCountStr := os.Getenv("SMF_COUNT")
 	if smfCountStr == "" {
@@ -189,7 +192,9 @@ func (i *_IDPool) allocate() (id int64, err error) {
 		logger.CtxLog.Errorf("failed to convert SMF_COUNT to int: %v", err)
 	}
 	for id = i.index; id <= i.maxValue; id++ {
+		logger.CtxLog.Infof("IDPool LOOP >>> trying id=%d (index=%d max=%d)", id, i.index, i.maxValue)
 		if _, exist := i.isUsed[id]; !exist {
+			logger.CtxLog.Infof("IDPool LOOP >>> id=%d already in use, continue", id)
 			i.isUsed[id] = true
 			i.index = (id % i.maxValue) + 1
 			if i.index == 1 {
@@ -203,8 +208,10 @@ func (i *_IDPool) allocate() (id int64, err error) {
 	}
 
 	logger.CtxLog.Debugf("IDPool: Wrapping around, checking IDs from 1 to %d", i.index-1)
+	logger.CtxLog.Infof("IDPool WRAP >>> index reset from %d to minValue=%d", i.index, i.minValue)
 
 	for id = int64((smfCount-1)*2500 + 1); id <= i.index; id++ {
+		logger.CtxLog.Infof("IDPool WRAP LOOP >>> trying id=%d", id)
 		if _, exist := i.isUsed[id]; !exist {
 			i.isUsed[id] = true
 			i.index = id + 1
@@ -214,7 +221,7 @@ func (i *_IDPool) allocate() (id int64, err error) {
 			logger.CtxLog.Infof("IDPool: ID %d already in use (wrap-around)", id)
 		}
 	}
-
+	logger.CtxLog.Infof("IDPool EXHAUSTED >>> no free IDs (used=%d maxValue=%d)", len(i.isUsed), i.maxValue)
 	logger.CtxLog.Infof("IDPool: No available value range to allocate ID")
 	return 0, errors.New("no available value range to allocate id")
 }
