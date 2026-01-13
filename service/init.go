c797d4726d439e90549b32d3b66f7a0afc7157e3
ReshmaJS <you@example.com>
2026-01-13 14:15:35 +0530

    lint error fix

--- service/init.go
+++ service/init.go
@@ -333,6 +333,7 @@ func (smf *SMF) Start() {
 
 	// Init UE Specific Config
 	context.InitSMFUERouting(&factory.UERoutingConfig)
+
 	//SetupSmfCollection ->so that db updated then newpool created will set index
 	if factory.SmfConfig.Configuration.EnableDbStore {
 		logger.InitLog.Infoln("SetupSmfCollection")
@@ -392,9 +393,7 @@ func (smf *SMF) Start() {
 			eventexposure.AddService(router)
 		}
 	}
-
 	//setupsmfcollection->later
-
 	// Init Kafka stream
 	if err := metrics.InitialiseKafkaStream(factory.SmfConfig.Configuration); err != nil {
 		logger.InitLog.Errorf("initialise kafka stream failed, %v ", err.Error())
