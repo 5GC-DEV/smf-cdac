4be33988505bc3aa6f58f9202eacc3508cec8862
ReshmaJS <you@example.com>
2026-01-15 11:42:26 +0530

    pool create flow chnage

--- service/init.go
+++ service/init.go
@@ -325,8 +325,22 @@ func (smf *SMF) Start() {
 		os.Exit(0)
 	}()
 
-	// Init SMF Service
+	// SetupSmfCollection->moves here
+	// Setup DB first (if enabled)
+	if factory.SmfConfig.Configuration.EnableDbStore {
+		logger.InitLog.Infoln("SetupSmfCollection")
+		context.SetupSmfCollection()
+	}
+
+	// Init SMF Context ONCE
 	smfCtxt := context.InitSmfContext(&factory.SmfConfig)
+
+	// Init DRSM only if DB enabled
+	if factory.SmfConfig.Configuration.EnableDbStore {
+		if err := smfCtxt.InitDrsm(); err != nil {
+			logger.InitLog.Errorf("initialise drsm failed, %v ", err.Error())
+		}
+	}
 
 	// allocate id for each upf
 	context.AllocateUPFID()
@@ -382,16 +396,7 @@ func (smf *SMF) Start() {
 		}
 	}
 
-	if factory.SmfConfig.Configuration.EnableDbStore {
-		logger.InitLog.Infoln("SetupSmfCollection")
-		context.SetupSmfCollection()
-		// Init DRSM for unique FSEID/FTEID/IP-Addr
-		if err := smfCtxt.InitDrsm(); err != nil {
-			logger.InitLog.Errorf("initialise drsm failed, %v ", err.Error())
-		}
-	} else {
-		logger.InitLog.Infoln("DB is disabled, not initialising drsm")
-	}
+	// set smf collection -> later
 
 	// Init Kafka stream
 	if err := metrics.InitialiseKafkaStream(factory.SmfConfig.Configuration); err != nil {
