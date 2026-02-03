3e1be8cb8da1959581d68ae47043aba0439b2722
anaswara <anaswara.n@cdac.in>
2026-02-03 15:08:43 +0530

    nul check added

--- context/sm_context.go
+++ context/sm_context.go
@@ -409,6 +409,10 @@ func GetSMContextBySEID(SEID uint64) (smContext *SMContext) {
 }
 
 func (smContext *SMContext) ReleaseUeIpAddr() error {
+	if smContext.PDUAddress == nil {
+		smContext.SubPduSessLog.Errorf("[ReleaseUeIpAddr] PDUAddress is nil, nothing to release")
+		return nil
+	}
 	if ip := smContext.PDUAddress.Ip; ip != nil && !smContext.PDUAddress.UpfProvided {
 		smContext.SubPduSessLog.Infof("Release IP[%s]", smContext.PDUAddress.Ip.String())
 		smContext.DNNInfo.UeIPAllocator.Release(smContext.Supi, ip)
