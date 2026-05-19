c766e6b47f93f2c39bcd4ca8a5fd27cdf13d450c
AshithaCDAC <ashitha@cdac.in>
2026-05-19 07:39:33 +0000

    unit test fail fix

--- metrics/telemetry.go
+++ metrics/telemetry.go
@@ -76,7 +76,7 @@ func initSmfStats() *SmfStats {
 		sessStats: prometheus.NewCounterVec(prometheus.CounterOpts{
 			Name: "smf_pdu_session_stats",
 			Help: "Counter of total session status",
-		}, []string{"smf_id", "msg_type", "result"}),
+		}, []string{"msg_type", "result"}),
 
 		sessRequest: prometheus.NewCounterVec(prometheus.CounterOpts{
 			Name: "smf_pdu_session_requests",
