cec046d2bfaaf2d9c3b5bfedc70ce6b53906148c
anaswara <anaswara.n@cdac.in>
2026-05-29 09:30:19 +0530

    code updated to local port

--- qos/qos_rule.go
+++ qos/qos_rule.go
@@ -509,7 +509,7 @@ func (pf *PacketFilter) GetPfContent(flowDesc string) {
 	}
 
 	// Remote Port
-	if pfc, len := buildPFCompPort(false, ipf.sPort); pfc != nil {
+	/*if pfc, len := buildPFCompPort(false, ipf.sPort); pfc != nil {
 		pfcList = append(pfcList, *pfc)
 		pf.ContentLength += len
 	}
@@ -518,10 +518,10 @@ func (pf *PacketFilter) GetPfContent(flowDesc string) {
 	if pfc, len := buildPFCompPortRange(false, ipf.sPortRange); pfc != nil {
 		pfcList = append(pfcList, *pfc)
 		pf.ContentLength += len
-	}
+	}*/
 
 	// Remote Port (temporarily use Local Port type for testing)
-	/*if pfc, len := buildPFCompPort(true, ipf.sPort); pfc != nil {
+	if pfc, len := buildPFCompPort(true, ipf.sPort); pfc != nil {
 		pfcList = append(pfcList, *pfc)
 		pf.ContentLength += len
 	}
@@ -530,7 +530,7 @@ func (pf *PacketFilter) GetPfContent(flowDesc string) {
 	if pfc, len := buildPFCompPortRange(true, ipf.sPortRange); pfc != nil {
 		pfcList = append(pfcList, *pfc)
 		pf.ContentLength += len
-	}*/
+	}
 
 	// Local Addr
 	if pfc, len := buildPFCompAddr(true, ipf.dAddrv4); pfc != nil {
